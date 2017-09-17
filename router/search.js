e8e5360f8abc53092caaa7bb8ea24c8392c5601d
Pragistyo Machmud <new.SOS.hope@outlook.com>
2017-09-17 17:46:31 +0700

    search complete?

--- router/search.js
+++ router/search.js
@@ -2,26 +2,99 @@ const express = require('express');
 const router  = express.Router()
 const model = require('../models');
 
-// router.get('/',(req,res)=>{
-//   res.render('search',{data:false,err_msg:false,title:'SEARCH PAGE'})
-// })
 
 router.get('/',(req,res)=>{
-  // res.send(req.query.name.length)
-  if(!req.query.name || req.query.name.length < 1){
-    res.render('search',{data:false,err_msg:false,title:'SEARCH PAGE'})
-  }else{
-    // res.send(req.query.name)
-    model.Item.findAll({
-      include:[model.Suppliers],
-      where:{
-        name : {$iLike: `%${req.query.name}%`}
+  if(!req.query.name && !req.query.min && !req.query.max){
+      res.render('search',{data:false,dataConjunction:false,err_msg:false,title:'SEARCH PAGE'})
+    }
+  if(req.query.name){
+    if(req.query.min){
+      if(req.query.max){
+        model.SupplierItem.findAll({include:[
+          {
+            model:model.Item,where:
+            {name:{$ilike:`%${req.query.name}%`}}
+          }
+          ,model.Suppliers],
+          where:{
+            price:{$between:[req.query.min,req.query.max]}
+          }
+          }).then(result=>{
+          // res.send(result)
+          res.render('search',{data:result,dataConjunction:false,err_msg:false,title:'SEARCH PAGE'})
+        })
       }
+      else if(!req.query.max){
+        // res.send('huahuahuaha')
+        model.SupplierItem.findAll({include:[
+          {
+            model:model.Item,where:
+            {name:{$ilike:`%${req.query.name}%`}}
+          }
+          ,model.Suppliers],
+          where:{
+            price:{$gte:req.query.min}
+          }
+          }).then(result=>{
+          // res.send(result)
+          res.render('search',{data:result,dataConjunction:false,err_msg:false,title:'SEARCH PAGE'})
+        })
+      }
+    }
+    else if(req.query.max){
+      // res.send('huahahahah')
+      model.SupplierItem.findAll({include:[
+        {
+          model:model.Item,where:
+          {name:{$ilike:`%${req.query.name}%`}}
+        }
+        ,model.Suppliers],
+        where:{
+          price:{$lte:req.query.max}
+        }
+        }).then(result=>{
+        // res.send(result)
+        res.render('search',{data:result,dataConjunction:false,err_msg:false,title:'SEARCH PAGE'})
+      })
+    }
+    model.SupplierItem.findAll({include:[
+      {
+        model:model.Item,where:
+        {name:{$ilike:`%${req.query.name}%`}}
+      }
+      ,model.Suppliers]
+      }).then(result=>{
+        res.render('search',{data:result,dataConjunction:false,err_msg:false,title:'SEARCH PAGE'})
     })
-    .then(result=>{
-      // res.send(result)
-      res.render('search',{data:result,err_msg:false,title:'SEARCH PAGE'})
-    })
+  }
+  if(!req.query.name){
+    if(req.query.min){
+      if(req.query.max){
+        model.SupplierItem.findAll({include:{all:true},where:{
+          price:{$between:[req.query.min,req.query.max]}
+        }})
+        .then(result=>{
+          res.render('search',{data:result,dataConjunction:false,err_msg:false,title:'SEARCH PAGE'})
+        })
+      }
+      else if(!req.query.max){
+        model.SupplierItem.findAll({include:{all:true},where:{
+          price:{$gte:req.query.min}
+        }})
+        .then(result=>{
+          res.render('search',{data:result,dataConjunction:false,err_msg:false,title:'SEARCH PAGE'})
+        })
+      }
+    }
+    else if(req.query.max){
+      // res.send('huahahha')
+      model.SupplierItem.findAll({include:{all:true},where:{
+        price:{$lte:req.query.max}
+      }})
+      .then(result=>{
+        res.render('search',{data:result,dataConjunction:false,err_msg:false,title:'SEARCH PAGE'})
+      })
+    }
   }
 })
 
