7f9bbcb1821b14fabae7967d3cab2c465d82b6c7
Pragistyo Machmud <new.SOS.hope@outlook.com>
2017-09-17 13:30:34 +0700

    still works on search validation

--- router/search.js
+++ router/search.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router  = express.Router()
+const model = require('../models');
+
+// router.get('/',(req,res)=>{
+//   res.render('search',{data:false,err_msg:false,title:'SEARCH PAGE'})
+// })
 
 router.get('/',(req,res)=>{
-  res.render('search',{err_msg:false,title:'SEARCH PAGE'})
+  // res.send(req.query.name.length)
+  if(req.query.name.length < 1){
+    res.render('search',{data:false,err_msg:false,title:'SEARCH PAGE'})
+  }else{
+    // res.send(req.query.name)
+    model.Item.findAll({
+      include:[model.Suppliers],
+      where:{
+        name : req.query.name
+      }
+    })
+    .then(result=>{
+      res.send(result)
+      // res.render('search',{data:result,err_msg:false,title:'SEARCH PAGE'})
+    })
+  }
 })
 
 module.exports =router
