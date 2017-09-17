638e674add0cfe03c6a65a25a5b1e3754fd4c44d
Pragistyo Machmud <new.SOS.hope@outlook.com>
2017-09-17 14:18:59 +0700

    add search feature for item name

--- router/search.js
+++ router/search.js
@@ -8,19 +8,19 @@ const model = require('../models');
 
 router.get('/',(req,res)=>{
   // res.send(req.query.name.length)
-  if(req.query.name.length < 1){
+  if(!req.query.name || req.query.name.length < 1){
     res.render('search',{data:false,err_msg:false,title:'SEARCH PAGE'})
   }else{
     // res.send(req.query.name)
     model.Item.findAll({
       include:[model.Suppliers],
       where:{
-        name : req.query.name
+        name : {$iLike: `%${req.query.name}%`}
       }
     })
     .then(result=>{
-      res.send(result)
-      // res.render('search',{data:result,err_msg:false,title:'SEARCH PAGE'})
+      // res.send(result)
+      res.render('search',{data:result,err_msg:false,title:'SEARCH PAGE'})
     })
   }
 })
